b04c1bdd5d26b22d4db9a041bbaf0e3125ee18ae
MAYER-Ti <s.lutov@inbox.ru>
2024-10-14 23:30:30 +0300

    add r4_counter

--- lab-5/JK-RStrig-vhdl/jk_rstrig_vhdl.vhd
+++ lab-5/JK-RStrig-vhdl/jk_rstrig_vhdl.vhd
@@ -10,36 +10,36 @@ PORT (
 	k : IN std_logic;
 
 	q  : OUT std_logic;
-	qi : OUT std_logic;
+	qi : OUT std_logic
 
-	q_temp : INOUT std_logic
+--	q_temp : INOUT std_logic
 );
 END jk_rstrig_vhdl;
 
 ARCHITECTURE behav OF jk_rstrig_vhdl IS
---SIGNAL q_s:std_logic := '0';
+SIGNAL q_s:std_logic := '0';
 BEGIN
 	PROCESS(s, j, r, k)
 	BEGIN
 			IF(r = '0') THEN
-				q_temp <= '0';
+				q_s <= '0';
 			ELSIF(s = '0') THEN
-				q_temp <= '1';
+				q_s <= '1';
 			ELSE
-				q_temp <= q_temp;
+				q_s <= q_s;
 			END IF;
---	END PROCESS;
---	PROCESS(k, j)
---	BEGIN
+
 			IF(k = '1') THEN
 				q <= '0';
 				qi <= '1';
+				q_s <= '0';
 			ELSIF(j = '1') THEN
 				q <= '1';
-				qi <= '0';				
+				qi <= '0';
+				q_s <= '1';				
 			ELSE
-				q <= q_temp;
-				qi <= NOT q_temp;
+				q <= q_s;
+				qi <= NOT q_s;
 			END IF;
     END PROCESS;
 END behav;
